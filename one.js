3e1c4c314ff58ccc385b1ddf759beccb8fe089cf
SIDDU REDDY <116141072+Madakalas@users.noreply.github.com>
2023-12-12 20:19:32 +0530

    Add files via upload

--- one.js
+++ one.js
@@ -1,10 +1,32 @@
-function sid()
-{
-    console.log("SidduGm")
-}
-sid()
+/* intiating methods + functions*/
+class One{
+    acc_Amount=0;
+    acc_Name;
+    acc_Id;
 
-let p = () =>
-{
-    console.log("SidduGm")}
+
+    open_Account(){
+        console.log("Account openesd successfully")
+    }
+    deposit_Amount(){
+        console.log("Amount should deposited successfully")
+
+    }
+    withdrawl(){
+        console.log("Insufficient to withdrawl")
+    }
+    get_Balance(amount){
+        this.acc_Amount=this.acc_Amount+amount;
+        
+    }
+    close_Account(){
+        console.log("Account closed succesfully")
+    }
+}
+let a = new One;
+a.open_Account();
+a.deposit_Amount();
+a.withdrawl();
+a.get_Balance(5000);
+a.close_Account();
 
