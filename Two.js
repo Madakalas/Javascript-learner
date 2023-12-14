0aaf0b14cf2ca89d8945816c6886ae57475f4d0d
SIDDU REDDY <116141072+Madakalas@users.noreply.github.com>
2023-12-14 15:24:31 +0530

    String Literal

--- Two.js
+++ Two.js
@@ -1,22 +1,4 @@
-/* Accesing object values*/
-class One{
-    constructor(Name,Id,Amount){
-        this.acc_Amount=Amount;
-        this.acc_Id=Id;
-        this.acc_Name=Name;
-
-    }
-    acc_Name;
-    acc_Id;
-    acc_Amount=0;
-
-    get_Balance(amount){
-        this.acc_Amount=this.acc_Amount+amount;   
-    }
-}
-let a = new One("rocky",101,40000);
-console.log(a);
-
-a.get_Balance(5000);
-
-
+let empid=101
+let ename="Rahul"
+console.log("Employee Id:", empid, ",Employee Name",ename)
+console.log(`Employee Id:${empid}, Employee Name: ${ename}`)
